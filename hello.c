ad9dc13bbf31ae529fd2d4eb85bc2b94d6932b05
jzhang707 <54588729+jzhang707@users.noreply.github.com>
2020-06-09 09:29:47 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,33 @@
+/**
+
+ * Author: Jialong Zhang
+
+ *         jialong.zhang@huskers.unl.edu
+
+ * Date: 2020/06/09
+
+ *
+
+ * A simple program abour names and majors in C
+
+ *
+
+ */
+
+#include <stdlib.h>
+
+#include <stdio.h>
+
+
+
+int main(int argc, char **argv) {
+
+
+
+  printf("Jialong Zhang\n");
+
+
+
+  return 0;
+
+}
